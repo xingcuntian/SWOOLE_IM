6514040ddb7133a261509e7ff99856cc52e51822
zhangxuanru <zhangxuanru@corp.1jiajie.com>
2016-05-12 15:45:49 +0800

    swoole app server

--- Client/apps/configs/db.php
+++ Client/apps/configs/db.php
@@ -1,12 +1,12 @@
 <?php
 $db['master'] = array(
-    'type'       => Swoole\Database::TYPE_MYSQLi,
+    'type'       => Swoole\Database::TYPE_MYSQL,
     'host'       => "127.0.0.1",
     'port'       => 3306,
     'dbms'       => 'mysql',
     'engine'     => 'MyISAM',
     'user'       => "root",
-    'passwd'     => "root",
+    'passwd'     => "",
     'name'       => "test",
     'charset'    => "utf8",
     'setname'    => true,
