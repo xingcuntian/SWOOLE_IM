41fc8b01c07fcb1edbd0056f4064cd0902442e7f
zhangxuanru <zhangxuanru@corp.1jiajie.com>
2016-05-13 15:16:22 +0800

    session is redis save

--- Client/apps/configs/cache.php
+++ Client/apps/configs/cache.php
@@ -3,6 +3,18 @@ $cache['session'] = array(
     'type' => 'FileCache',
     'cache_dir' => WEBPATH.'/cache/filecache/',
 );
+/* session save is  redis
+
+$cache['session'] = array(
+    'type' => 'Redis',
+    'host'    => "127.0.0.1",
+    'port'    => 6379,
+    'password' => '',
+    'timeout' => 0.25,
+    'pconnect' => false
+);
+ * */
+
 $cache['master'] = array(
     'type' => 'Memcache',
 );
