17607656c27c0f2248ba81cdc90f63289c0296d7
zhangxuanru <zhangxuanru@corp.1jiajie.com>
2016-05-13 15:39:59 +0800

    add admin controller

---
+++ Client/apps/controllers/Admin.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+use Swoole;
+
+class Admin extends Swoole\Controller
+{
+    function index()
+    {
+        $this->session->start();
+        var_dump($_SESSION);
+        Swoole\Auth::loginRequire();
+        $this->display('admin/index.php');
+    }
+
+
+    function logout()
+    {
+        $this->session->start();
+        $this->user->logout();
+    }
+
+    public  function logint()
+    {
+        $this->display('user/logint.php');
+    }
+}
